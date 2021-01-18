4da6537aec8fd50e89428069abbd2e985a5424a2
LJKR2020 <loredana.rietveld@gmail.com>
2021-01-19 00:36:09 +0100

    opdracht 1 en 3

--- array-loops.js
+++ array-loops.js
@@ -3,32 +3,43 @@
 // --------------------------
 
 const names = ["Henk", "Piet", "Fred", "Joop"];
-names = names + "je"
-console.log(names)
+names[0] = names[0] + "je";
+names[1] = names[1] + "je";
+names[2] = names[2] + "je";
+names[3] = names[3] + "je";
+console.log(names);
 
 // Verander de waardes van de array dusdanig dat er -"je" achter iedere naam wordt geplakt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
 // console.log(names) geeft: ["Henkje", "Pietje", "Fredje", "Joopje"]
-
-
 
 // --------------------------
 // Opdracht 2
 // --------------------------
 
 const numbers = [2, 4, 5, 29, 38];
+numbers[0] = numbers[0] * 3;
+numbers[1] = numbers[1] * 3;
+numbers[2] = numbers[2] * 3;
+numbers[3] = numbers[3] * 3;
+numbers[4] = numbers[4] * 3;
+console.log(numbers);
 
 // Verander de waardes van de array dusdanig dat iedere waarde met 3 vermenigvuldigd wordt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
 // console.log(numbers) geeft: [ 6, 12, 15, 87, 114 ];
-
-
 
 // --------------------------
 // Opdracht 3
 // --------------------------
 
 const figures = [4, 6, 12, 34, 1];
+figures[0] = figures[0] + 3;
+figures[1] = figures[1] + 3;
+figures[2] = figures[2] + 3;
+figures[3] = figures[3] + 3;
+figures[4] = figures[4] + 3;
+console.log(figures);
 
 // Verander de waardes van de array dusdanig dat er 3 bij iedere waarde wordt opgetelt
 // Wanneer je de volledige array logt zie je alle nieuwe waardes
